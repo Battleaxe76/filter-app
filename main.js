59173f0b55c88df419b9bc7633cc5f1bf0c2c388
Battleaxe76 <70934717+Battleaxe76@users.noreply.github.com>
2020-10-27 16:19:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ function preload(){
 }
 function setup(){
     canvas=createCanvas(300,300);
-    canvas.center() 
+    canvas.center() ;
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
 }
 function draw(){
-    fill('red');
-    stroke('blue')
-circle(200,200,50)
+ image(video,0,0,300,300)
 
 }
 function take_snapshot(){
 save("MyFilterPhoto");
 }
+function modelLoaded(){
+    console.log('pose net is intialized');
+}
+function gotPoses(results){
+if (results.length>0){
+    console.log(results);
+    console.log("noseX="+results [0].pose.nose.x);
+    console.log("noseY="+results [0].pose.nose.y);
+}
+}
+
